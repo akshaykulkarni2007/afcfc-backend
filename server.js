809cc8472e31ccd8099e7f4a384ced17bc0fa09b
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2018-07-19 23:48:07 +0530

    cors + remove ;

--- server.js
+++ server.js
@@ -1,35 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const passport = require('passport');
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const passport = require("passport")
+cors = require('cors')
 
 // Routes
-const users = require('./routes/api/users');
+const users = require("./routes/api/users")
 
-const app = express();
+const app = express()
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI
 
 // Connect to MongoDB
 mongoose
-  .connect(db)
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+	.connect(db)
+	.then(() => console.log("MongoDB Connected"))
+	.catch(err => console.log(err))
 
 // Passport middleware
-app.use(passport.initialize());
+app.use(passport.initialize())
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport)
+
+app.use(cors())
 
 // Use Routes
-app.use('/api/users', users);
+app.use("/api/users", users)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
